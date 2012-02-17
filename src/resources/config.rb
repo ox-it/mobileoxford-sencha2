660fbb8c30338237717c49b783c6373edbc8b856
Martin Filliau <martin@filliau.com>
2012-02-17 14:46:25 +0000

    added theming with sass

---
+++ src/resources/config.rb
@@ -0,0 +1,9 @@
+dir = File.dirname(__FILE__)
+
+load File.join(dir, '..', '..', '..', 'sencha-touch', 'resources', 'themes')
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
